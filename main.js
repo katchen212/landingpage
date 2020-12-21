ba56dd998dd15efcd94dff5c1cc4e4c3955bb61a
katchen212 <44990409+katchen212@users.noreply.github.com>
2020-12-20 20:51:53 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ $('.header').height($(window).height());
+ 
+})
+
+/*header style*/
+.header{
+ background-image: url('../images/headerback.jpg');
+ background-attachment: fixed;
+ background-size: cover;
+ background-position: center;
+}
